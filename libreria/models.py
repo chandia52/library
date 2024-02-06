b7965886c725601dcef7088ff5302da1c1ae521d
chandia52 <jorgechandia7@gmail.com>
2024-02-06 12:57:25 -0300

    Carga de archivos

---
+++ libreria/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Libro(models.Model):
+    id= models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100,verbose_name="Titulo")
+    imagen = models.ImageField(upload_to='imagenes/',verbose_name="Imagen", null=True)
+    descripcion = models.TextField(verbose_name="Descripcion",null=True)
+    def __str__(self):
+        fila = "Titulo: " + self.titulo + " |||| " + "Descripción: " + self.descripcion
+        return fila
+    def delete(self,using=None,keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+        
